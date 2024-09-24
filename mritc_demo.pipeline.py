2865fed6de46ecc5d11c44f3cddc4ab52664a5f2
Chris Jackett <chris.jackett@csiro.au>
2024-09-24 15:44:54 +1000

    Refactor MRITC pipeline and remove .gitignore
    Deleted the .gitignore file and renamed the MRITC pipeline class to MRITCDemoPipeline. Refactored methods to streamline the import, processing, and packaging by simplifying directory traversals and other logic.

--- mritc_demo.pipeline.py
+++ mritc_demo.pipeline.py
@@ -13,10 +13,7 @@ from marimba.core.pipeline import BasePipeline
 from marimba.lib import image
 
 
-class MRITCPipeline(BasePipeline):
-    """
-    Test pipeline. No-op.
-    """
+class MRITCDemoPipeline(BasePipeline):
 
     @staticmethod
     def get_pipeline_config_schema() -> dict:
@@ -35,31 +32,26 @@ class MRITCPipeline(BasePipeline):
         }
 
     def _import(
-        self,
-        data_dir: Path,
-        source_paths: List[Path],
-        config: Dict[str, Any],
-        **kwargs: dict,
+            self,
+            data_dir: Path,
+            source_path: Path,
+            config: Dict[str, Any],
+            **kwargs: dict,
     ):
-        self.logger.info(f"Importing data from {source_paths=} to {data_dir}")
-        for source_path in source_paths:
-            if not source_path.is_dir():
-                continue
+        self.logger.info(f"Importing data from {source_path=} to {data_dir}")
 
-            for source_file in source_path.glob("**/*"):
-                if source_file.is_file() and source_file.suffix.lower() in [
-                    ".csv",
-                    ".jpg",
-                    ".mp4",
-                ]:
-                    if not self.dry_run:
-                        copy2(source_file, data_dir)
-                    self.logger.debug(
-                        f"Copied {source_file.resolve().absolute()} -> {data_dir}"
-                    )
+        for source_file in source_path.rglob("*"):
+            if source_file.is_file() and source_file.suffix.lower() in [
+                ".csv",
+                ".jpg",
+                ".mp4",
+            ]:
+                if not self.dry_run:
+                    copy2(source_file, data_dir)
+                self.logger.debug(f"Copied {source_file.resolve().absolute()} -> {data_dir}")
 
     def get_image_output_file_name(
-        self, deployment_config: dict, file_path: Path, index: int
+            self, deployment_config: dict, file_path: Path, index: int
     ) -> str:
         try:
             image = Image.open(file_path)
@@ -95,15 +87,20 @@ class MRITCPipeline(BasePipeline):
                 f"Error: Unable to open {file_path}. Are you sure it's an image?"
             )
 
-    def _process(self, data_dir: Path, config: Dict[str, Any], **kwargs: dict):
+    def _process(
+            self,
+            data_dir: Path,
+            config: Dict[str, Any],
+            **kwargs: dict
+    ):
         jpg_list = []
 
-        for file in data_dir.glob("**/*"):
+        for file in data_dir.rglob("*"):
             if (
-                file.is_file()
-                and file.suffix.lower() in [".jpg"]
-                and "_THUMB" not in file.name
-                and "overview" not in file.name
+                    file.is_file()
+                    and file.suffix.lower() in [".jpg"]
+                    and "_THUMB" not in file.name
+                    and "overview" not in file.name
             ):
                 stills_path = data_dir / "stills"
                 stills_path.mkdir(exist_ok=True)
@@ -153,73 +150,76 @@ class MRITCPipeline(BasePipeline):
         )
         image.create_grid_image(thumb_list, data_dir / "overview.jpg")
 
-    def _compose(
-        self, data_dirs: List[Path], configs: List[Dict[str, Any]], **kwargs: dict
+    def _package(
+            self,
+            data_dir: Path,
+            config: Dict[str, Any],
+            **kwargs: dict
     ) -> Dict[Path, Tuple[Path, List[ImageData]]]:
-        data_mapping = {}
-        for data_dir, config in zip(data_dirs, configs):
-            file_paths = []
-            file_paths.extend(data_dir.glob("**/*"))
-            base_output_path = Path(config.get("deployment_id"))
 
-            sensor_data_df = pd.read_csv(next((data_dir / "data").glob("*.CSV")))
-            sensor_data_df["FinalTime"] = pd.to_datetime(
-                sensor_data_df["FinalTime"], format="%Y-%m-%d %H:%M:%S.%f"
-            ).dt.floor("S")
+        data_mapping: Dict[Path, Tuple[Path, List[ImageData]]] = {}
+        file_paths = []
+        file_paths.extend(data_dir.glob("**/*"))
+        base_output_path = Path(config.get("deployment_id"))
 
-            for file_path in file_paths:
-                output_file_path = base_output_path / file_path.relative_to(data_dir)
+        sensor_data_df = pd.read_csv(next((data_dir / "data").glob("*.CSV")))
+        sensor_data_df["FinalTime"] = pd.to_datetime(
+            sensor_data_df["FinalTime"], format="%Y-%m-%d %H:%M:%S.%f"
+        ).dt.floor("S")
 
-                if (
+        for file_path in file_paths:
+            output_file_path = base_output_path / file_path.relative_to(data_dir)
+
+            if (
                     file_path.is_file()
                     and file_path.suffix.lower() in [".jpg"]
                     and "_THUMB" not in file_path.name
                     and "overview" not in file_path.name
-                ):
-                    iso_timestamp = file_path.name.split("_")[5]
-                    target_datetime = pd.to_datetime(
-                        iso_timestamp, format="%Y%m%dT%H%M%SZ"
-                    )
-                    matching_rows = sensor_data_df[
-                        sensor_data_df["FinalTime"] == target_datetime
+            ):
+                iso_timestamp = file_path.name.split("_")[5]
+                target_datetime = pd.to_datetime(
+                    iso_timestamp, format="%Y%m%dT%H%M%SZ"
+                )
+                matching_rows = sensor_data_df[
+                    sensor_data_df["FinalTime"] == target_datetime
                     ]
 
-                    if not matching_rows.empty:
-                        # in iFDO, the image data list for an image is a list containing single ImageData
-                        image_data_list = [
-                            ImageData(
-                                image_datetime=datetime.strptime(
-                                    iso_timestamp, "%Y%m%dT%H%M%SZ"
-                                ),
-                                image_latitude=matching_rows["UsblLatitude"].values[0],
-                                image_longitude=float(
-                                    matching_rows["UsblLongitude"].values[0]
-                                ),
-                                image_depth=float(matching_rows["Altitude"].values[0]),
-                                image_altitude=float(
-                                    matching_rows["Altitude"].values[0]
-                                ),
-                                image_event=str(matching_rows["Operation"].values[0]),
-                                image_platform=self.config.get("platform_id"),
-                                image_sensor=str(matching_rows["Camera"].values[0]),
-                                image_camera_pitch_degrees=float(
-                                    matching_rows["Pitch"].values[0]
-                                ),
-                                image_camera_roll_degrees=float(
-                                    matching_rows["Roll"].values[0]
-                                ),
-                                image_uuid=str(uuid4()),
-                                # image_pi=self.config.get("voyage_pi"),
-                                image_creators=[],
-                                image_license="MIT",
-                                image_copyright="",
-                                image_abstract=self.config.get("abstract"),
-                            )
-                        ]
+                if not matching_rows.empty:
+                    # in iFDO, the image data list for an image is a list containing single ImageData
+                    image_data_list = [
+                        ImageData(
+                            image_datetime=datetime.strptime(
+                                iso_timestamp, "%Y%m%dT%H%M%SZ"
+                            ),
+                            image_latitude=matching_rows["UsblLatitude"].values[0],
+                            image_longitude=float(
+                                matching_rows["UsblLongitude"].values[0]
+                            ),
+                            image_depth=float(matching_rows["Altitude"].values[0]),
+                            image_altitude=float(
+                                matching_rows["Altitude"].values[0]
+                            ),
+                            image_event=str(matching_rows["Operation"].values[0]),
+                            image_platform=self.config.get("platform_id"),
+                            image_sensor=str(matching_rows["Camera"].values[0]),
+                            image_camera_pitch_degrees=float(
+                                matching_rows["Pitch"].values[0]
+                            ),
+                            image_camera_roll_degrees=float(
+                                matching_rows["Roll"].values[0]
+                            ),
+                            image_uuid=str(uuid4()),
+                            # image_pi=self.config.get("voyage_pi"),
+                            image_creators=[],
+                            image_license="MIT",
+                            image_copyright="",
+                            image_abstract=self.config.get("abstract"),
+                        )
+                    ]
 
-                        data_mapping[file_path] = output_file_path, image_data_list
+                    data_mapping[file_path] = output_file_path, image_data_list
 
-                elif file_path.is_file():
-                    data_mapping[file_path] = output_file_path, None
+            elif file_path.is_file():
+                data_mapping[file_path] = output_file_path, None, None
 
         return data_mapping
